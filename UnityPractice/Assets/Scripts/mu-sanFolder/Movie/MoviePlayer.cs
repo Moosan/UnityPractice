b46990c40fcbdc3ed0b6b5bb174f2e01d1b41c52
Moosan <washer2016@outlook.jp>
2017-03-09 23:54:38 +0900

    フォルダ構造変えた

---
+++ UnityPractice/Assets/Scripts/mu-sanFolder/Movie/MoviePlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoviePlayer : MonoBehaviour {
+
+	// Use this for initialization
+    protected void Start () {
+        var movieTexture=(MovieTexture)GetComponent<Renderer>().material.mainTexture;
+        movieTexture.Play();
+    }
+}
